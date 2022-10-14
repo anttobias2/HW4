209c88f65347b65e1ee435d8bf2e5210b45b15c3
Anthony Tobias <aelias2001@msn.com>
2022-10-14 11:22:19 -0700

    all questions added

--- script.js
+++ script.js
@@ -98,21 +98,116 @@ const questions = [
         ]
     },
     {
-    question: 'Who holds the MLB single season record for homeruns?',
+        question: 'Who holds the MLB single season record for homeruns?',
         answers: [
             {text: 'Barry Bonds', correct: true},
             {text: 'Babe Ruth', correct: false},
             {text: 'Hank Aaron', correct: false},
             {text: 'Ken Griffey Jr.', correct: false}
+    ]
+    },
+    {
+        question: 'What is the diameter of a basketball hoop?',
+        answers: [
+            {text: '18 inches', correct: true},
+            {text: '16 inches', correct: false},
+            {text: '24 inches', correct: false},
+            {text: '20 inches', correct: false}
         ]
     },
     {
-        question: 'What year were the OKC Thunder established',
+        question: 'How many dimples does the average golf ball have?',
         answers: [
-            {text: 'John Stockton', correct: true},
-            {text: 'Russell Westbrook', correct: false},
-            {text: 'Chris Paul', correct: false},
-            {text: 'Oscar Robertson', correct: false}
+            {text: '336', correct: true},
+            {text: '296', correct: false},
+            {text: '378', correct: false},
+            {text: '402', correct: false}
+        ]
+    },
+    {
+        question: 'What sporting event is held every year on memorial day?',
+        answers: [
+            {text: 'Indianapolis 500', correct: true},
+            {text: 'MLB Opening Day', correct: false},
+            {text: 'NBA Allstar Game', correct: false},
+            {text: 'The SuperBowl', correct: false}
+        ]
+    },
+    {
+        question: 'How long is an Olympic-sized swimming pool?',
+        answers: [
+            {text: '50 meters', correct: true},
+            {text: '45 meters', correct: false},
+            {text: '48 meters', correct: false},
+            {text: '52 meters', correct: false}
+        ]
+    },
+    {
+        question: 'How many defensive players are in the field during a baseball game?',
+        answers: [
+            {text: '9', correct: true},
+            {text: '8', correct: false},
+            {text: '10', correct: false},
+            {text: '11', correct: false}
+        ]
+    },
+    {
+        question: 'What is the object hit in Badminton called?',
+        answers: [
+            {text: 'Shuttlecock', correct: true},
+            {text: 'Badminton Ball', correct: false}
+        ]
+    },
+    {
+        question: 'How much does an NFL football weigh?',
+        answers: [
+            {text: '1 pound', correct: true},
+            {text: '1.2 pounds', correct: false},
+            {text: '0.8 pounds', correct: false},
+            {text: '1.5 pounds', correct: false}
+        ]
+    },
+    {
+        question: 'How far is the NBA 3-point line?',
+        answers: [
+            {text: '23 feet 9 inches', correct: true},
+            {text: '23 feet', correct: false},
+            {text: '24 feet 6 inches', correct: false},
+            {text: '25 feet', correct: false}
+        ]
+    },
+    {
+        question: 'Who is the all time NBA points leader?',
+        answers: [
+            {text: 'Kareem Abdul-Jabbar', correct: true},
+            {text: 'LeBron James', correct: false},
+            {text: 'Michael Jordan', correct: false},
+            {text: 'Tim Duncan', correct: false}
+        ]
+    },
+    {
+        question: 'Which team bats first in baseball?',
+        answers: [
+            {text: 'Away Team', correct: true},
+            {text: 'Home Team', correct: false}
+        ]
+    },
+    {
+        question: 'What sport is considered the \"King of sports\"?',
+        answers: [
+            {text: 'Soccer', correct: true},
+            {text: 'Baseball', correct: false},
+            {text: 'Basketball', correct: false},
+            {text: 'Football', correct: false}
+        ]
+    },
+    {
+        question: 'Which of the following is no longer a Formula 1 team?',
+        answers: [
+            {text: 'Renault', correct: true},
+            {text: 'Red Bull', correct: false},
+            {text: 'Mercedes', correct: false},
+            {text: 'Alpine', correct: false}
         ]
     }
 ]
